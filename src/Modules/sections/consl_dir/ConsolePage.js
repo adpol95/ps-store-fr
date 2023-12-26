ea53216d598e4fd8a5f7490ce84b07fae3aa6460
adpol95 <adpol95@gmail.com>
2023-12-26 18:31:44 +0300

    Consoles page view is done. It's still just a concept view, work in progress...

--- src/Modules/sections/consl_dir/ConsolePage.js
+++ src/Modules/sections/consl_dir/ConsolePage.js
@@ -1,16 +1,61 @@
 import {useLocation} from "react-router-dom";
+import {useState} from "react";
 
 function ConsolePage() {
     let {state} = useLocation();
     let titles = Object.keys(state.dataAboutGame[1]);
     let datas = Object.values(state.dataAboutGame[1]);
-    console.log(state)
+    let [currentPage, setCurrentPage] = useState(0);
     return (
-        <div>
+        <div onClick={(event) => {
+            if (event.target.innerText === '<' && currentPage > 0) {
+                setCurrentPage(currentPage - 1)
+            } else if (event.target.innerText === '>' && currentPage !== datas[1].length - 1) {
+                setCurrentPage(currentPage + 1)
+            }
+        }}>
             <h1>
                 {state.dataAboutGame[0]}
-           </h1>
-           <img src={datas.shift()} alt="" style={{width: "300px"}}/>
+            </h1>
+            <button>
+                &lt;
+            </button>
+            <img src={datas[1][currentPage]} alt="" style={{width: "300px"}}/>
+            <button>
+                &gt;
+            </button>
+            <div>{datas[3]}</div>
+            <div>Realise date: {datas[2]}</div>
+            <hr/>
+            <div>
+                <h3>Tearms</h3>
+                <ul>
+                    {datas[4].map((el, i) => <li key={i * 14}>{el}</li>)}
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    {state.dataAboutGame[1].mainText ? datas[5].map((el, i) => <li key={i * 114}>
+                            <img src={datas[1][i + 1]} alt="console img error"/>
+                        <h3>
+                            {el.title}
+                        </h3>
+                        <ul>
+                            {el.descript.map((el, il) => <li key={il * 221}>{el}</li>)}
+                        </ul>
+                    </li>
+                    ) : ""}
+                </ul>
+            </div>
+            <div>
+
+                <h3>
+                    {state.dataAboutGame[1].whatInTheBox ? 'What in the box' : ''}
+                </h3>
+                <ul>
+                    {state.dataAboutGame[1].whatInTheBox ? datas[6].map((el, i) => <li key={i * 25}> {el}</li>) : ""}
+                </ul>
+            </div>
         </div>
     )
 }
